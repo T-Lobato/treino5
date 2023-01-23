45ceeac7abe0606c8bb6558808a9bfb3fb7f26d0
T-Lobato <thyagollobato@gmail.com>
2023-01-23 15:16:17 -0300

    feat: Crud básico para a entidade Curso finalizado

--- src/main/java/base/api/treino5/controller/CursoController.java
+++ src/main/java/base/api/treino5/controller/CursoController.java
@@ -6,7 +6,11 @@ import base.api.treino5.service.CursoService;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -26,5 +30,21 @@ public class CursoController {
         return mapper.map(cursoService.save(cursoRequestDto), CursoResponseDto.class);
     }
 
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public CursoResponseDto findById(@PathVariable Long id) {
+        return mapper.map(cursoService.findById(id), CursoResponseDto.class);
+    }
 
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public CursoResponseDto update(@PathVariable Long id, @RequestBody CursoRequestDto cursoRequestDto) {
+        return mapper.map(cursoService.update(cursoRequestDto, id), CursoResponseDto.class);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) {
+        cursoService.deleteById(id);
+    }
 }
